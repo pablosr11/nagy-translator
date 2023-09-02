91b35a9053793e884c1d0a16f65d039c74de4491
ps <pablosr11@gmail.com>
2023-09-02 08:43:27 +0100

    add forms from shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+import { TextareaForm } from "@/components/textareaform";
+
 export default function Home() {
   return (
-    <main>
-      <h1>hello</h1>
-    </main>
+    <section className="py-10 sm:py-16 lg:py-24">
+      <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <div className="grid items-center grid-cols-1 gap-4 lg:grid-cols-2">
+          <TextareaForm
+            name="Magyar Nyelv"
+            placeholder="Kezdj el írni a fordításhoz"
+          />
+          <TextareaForm
+            name="Deutsch"
+            placeholder="Beginnen Sie mit dem Schreiben, um zu übersetzen"
+          />
+        </div>
+      </div>
+    </section>
   );
 }
---
+++ components/textareaform.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Textarea } from "@/components/ui/textarea";
+import { toast } from "@/components/ui/use-toast";
+
+const FormSchema = z.object({
+  name: z
+    .string()
+    .min(3, {
+      message: "Text must be at least 3 characters.",
+    })
+    .max(500, {
+      message: "Bio must be at most 500 characters.",
+    }),
+});
+
+export function TextareaForm({
+  name,
+  placeholder,
+}: {
+  name: string;
+  placeholder: string;
+}) {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    });
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>{name}</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder={placeholder}
+                  className="resize-none"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
