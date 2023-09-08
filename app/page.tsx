c2044861677f5ba4be83a24cf48d38eb5de5b9c4
ps <pablosr11@gmail.com>
2023-09-08 09:46:32 +0100

    screen

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
           setAnswer={setAnswer}
         />
       </div>
-      <ScrollArea className="h-[200px] w-[300px] lg:w-[800px] lg:h-[300px]  my-3 px-4 py-4 mx-auto max-w-7xl sm:px-6 lg:px-8 bg-teal-100 rounded-md p-4">
+      <ScrollArea className="h-[300px] w-[300px] lg:w-[800px] lg:h-[300px]  my-3 px-4 py-4 mx-auto max-w-7xl sm:px-6 lg:px-8 bg-teal-100 rounded-md p-4">
         {/* show translation if its not undefined and answer its not empty string */}
         {answer && (
           <div className="text-center grid grid-cols-2 gap-4">
